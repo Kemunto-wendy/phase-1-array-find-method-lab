0014354a16cafdf9f13e73d3278d76a5ea76936b
JOY <joy.kemunto@student.moringaschool.com>
2022-06-12 11:31:16 +0300

    commit tree e4a39e0088196f13eeea7bf4103e5ad10383011e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+// describe('superbowlWin(record)', function () {}
+superbowlWin = (record) => {
+
+    const result = record.find( record => record.result === "W" );
+    
+    return !!result ? result.year : undefined;
+  }
